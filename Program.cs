782524d0f991e292d4d536e2077c1d5200412e6a
Роман Иванов <romaiv99@gmail.com>
2022-06-13 02:04:34 +0300

    Выполнение задания

--- Program.cs
+++ Program.cs
@@ -7,11 +7,35 @@ namespace Logger
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Pathfinder pathfinder1 = new Pathfinder(new FileLogWritter());
+            Pathfinder pathfinder2 = new Pathfinder(new ConsoleLogWritter());
+            Pathfinder pathfinder3 = new Pathfinder(new SecureConsoleLogWritter());
+            Pathfinder pathfinder4 = new Pathfinder(new SecureFileLogWritter());
+            Pathfinder pathfinder5 = new Pathfinder(new SpecialFileLogWritter());
         }
     }
 
-    class ConsoleLogWritter
+    class Pathfinder
+    {
+        private ILogger _logger;
+
+        public Pathfinder(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Find(string message)
+        {
+            _logger.WriteError(message);
+        }
+    }
+
+    interface ILogger
+    {
+        public void WriteError(string message);
+    }
+
+    class ConsoleLogWritter: ILogger
     {
         public virtual void WriteError(string message)
         {
@@ -19,7 +43,7 @@ namespace Logger
         }
     }
 
-    class FileLogWritter
+    class FileLogWritter: ILogger
     {
         public virtual void WriteError(string message)
         {
@@ -27,7 +51,7 @@ namespace Logger
         }
     }
 
-    class SecureConsoleLogWritter : ConsoleLogWritter
+    class SecureConsoleLogWritter : ConsoleLogWritter, ILogger
     {
         public override void WriteError(string message)
         {
@@ -35,6 +59,26 @@ namespace Logger
             {
                 base.WriteError(message);
             }
+        }
+    }
+
+    class SecureFileLogWritter : FileLogWritter, ILogger
+    {
+        public override void WriteError(string message)
+        {
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Friday)
+            {
+                base.WriteError(message);
+            }
+        }
+    }
+
+    class SpecialFileLogWritter : SecureFileLogWritter, ILogger
+    {
+        public override void WriteError(string message)
+        {
+            Console.WriteLine(message);
+            base.WriteError(message);
         }
     }
 }
